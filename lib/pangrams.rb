c27478a17dee43646ea6b93c66f27c341c31a888
Jose Clavero <joseclamo@gmail.com>
2018-03-01 23:09:56 +0000

    pangrams_solution

--- lib/pangrams.rb
+++ lib/pangrams.rb
@@ -1,3 +1,15 @@
+
 def is_pangram?(sentence)
-  
+
+    sentence = sentence.downcase
+    sentence_clean = sentence.gsub(/[áéíóú]/, 'á' => 'a', 'é' => 'e', 'í' => 'i', 'ó' => 'o', 'ú' => 'u')
+    abc = "qwertyuioplkjhgfdsazxcvbnm"
+
+    result = true
+    abc.each_char do |letter|
+        unless sentence_clean.include? letter
+            result = false
+        end
+    end
+    return result
 end
