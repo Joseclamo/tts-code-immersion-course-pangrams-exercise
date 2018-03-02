a38d0785f54d598e0cad904512aca244be288156
Jose Clavero <joseclamo@gmail.com>
2018-03-02 10:18:05 +0000

    implem

--- lib/pangrams.rb
+++ lib/pangrams.rb
@@ -1,13 +1,11 @@
-
 def is_pangram?(sentence)
 
-    sentence = sentence.downcase
-    sentence_clean = sentence.gsub(/[áéíóú]/, 'á' => 'a', 'é' => 'e', 'í' => 'i', 'ó' => 'o', 'ú' => 'u')
+    sentence = sentence.downcase.gsub(/[áéíóú]/, 'á' => 'a', 'é' => 'e', 'í' => 'i', 'ó' => 'o', 'ú' => 'u')
     abc = "qwertyuioplkjhgfdsazxcvbnm"
 
     result = true
     abc.each_char do |letter|
-        unless sentence_clean.include? letter
+        unless sentence.include? letter
             result = false
         end
     end
